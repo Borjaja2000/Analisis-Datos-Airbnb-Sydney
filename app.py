0c1734987c903deb327dc8cfe32aef8b97ce6bfd
Borjaja2000 <46570584+Borjaja2000@users.noreply.github.com>
2024-06-30 17:33:16 +0200

    commit

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import matplotlib.pyplot as plt
+import vaex
 
 import folium
 from folium.plugins import FastMarkerCluster
@@ -249,7 +250,8 @@ elif pestaña == "Importancia del Vecindario":
         fig.update_layout(legend=dict(orientation="h", y=1.06, x=0, xanchor='left'))
         st.plotly_chart(fig, use_container_width=True)
     with tabsVecindario[2]:
-        calendar_data = pd.read_csv("Recursos/calendar.csv", compression="gzip")
+        calendar_data = vaex.from_csv("Recursos/calendar.csv", convert=True, chunk_size=5_000_000)
+        
         calendar_data = pd.merge(listings, calendar_data, left_on="id", right_on="listing_id", how="left")
         calendar_data = calendar_data.groupby(["neighbourhood", "date"])["available"].value_counts().unstack()
         calendar_data["available_ratio"] = np.round(calendar_data["t"] / (calendar_data["t"] + calendar_data["f"]) * 100, 2)
